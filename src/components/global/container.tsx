bb6fe6cceecb29c633ba45c46235338a890558c1
unknown <shakhawatsalam726@gmail.com>
2024-08-20 00:41:56 +0530

    [NavBar is done]

---
+++ src/components/global/container.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+  className: string;
+}
+
+const Container = () => {
+  return <div>Container</div>;
+};
+
+export default Container;
